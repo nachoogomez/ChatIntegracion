6dab27bdc6bce3121d3252446b0f8d1271dbccde
Ignacio <nachogomez09@gmail.com>
2024-06-19 14:41:02 -0300

    Second commit

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,4 +1,3 @@
-import { log } from 'console';
 import express from 'express';
 import http from 'http';
 import {Server} from 'socket.io';
@@ -7,12 +6,15 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', socket =>{
+io.on('connection', (socket) =>{
     console.log('Client connected')
 
-    socket.on('message', (message)=>{
-        console.log(message)
-        socket.broadcast.emit('message', message)
+    socket.on('message', (body) => {
+        console.log(socket.id);
+        socket.broadcast.emit('message', {
+            body,
+            from: socket.id.slice(4)
+        })
     })
 });
     
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import Form from '../components/form/Form'
-import './App.css'
+import Form from './components/form/Form'
+
 
 function App() {
   
 
   return (
     <>
-      <Form/>
+      <Form />
     </>
   )
 }
